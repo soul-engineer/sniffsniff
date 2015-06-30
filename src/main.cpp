c3dbfad0d85d25c64ab685597241481c6ac886a4
Artem.Nazyrov <avnazyrov@yandex.ru>
2015-06-30 17:30:10 +0400

    main: moved provider to separate thread, added signals handler

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,29 @@
 #include "dispatcher.h"
 #include <stdexcept>
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <csignal>
+
+
+namespace
+{
+    const auto SLEEP_MS = 1;
+    std::atomic<bool> running_ {true};
+
+    void sig_handler(int signo)
+    {
+        switch (signo)
+        {
+            case SIGINT:
+            case SIGTERM:
+                running_.store(false, std::memory_order_relaxed);
+                break;
+            default:;
+        }
+    }
+}
 
 
 int main(int argc, char** argv)
@@ -19,15 +42,35 @@ int main(int argc, char** argv)
     {
         google::InitGoogleLogging(argv[0]);
 
-        // TODO: this code is for quick test only
         // Parse program options
         config cfg;
         cfg.init(argc, argv);
         
+        // Configure signals handling
+        LOG_IF(FATAL, signal(SIGINT, sig_handler) == SIG_ERR) <<
+                "Can't configure signal handling";
+        
+        // Create dispatcher
         dispatcher disp(cfg);
+        
+        // Configure frame capturer...
         provider prov(disp);
         prov.open_iface(cfg);
-        prov.run();
+        // ...and run it in a separate thread
+        std::thread prov_thread(&provider::run, &prov);
+        
+        // Main cycle
+        while (running_.load(std::memory_order_relaxed))
+        {
+            // TODO: Examine worker results and log it
+            std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_MS));
+        }
+
+        // Wait for provider to stop
+        prov.stop();
+        prov_thread.join();
+        
+        LOG(INFO) << "Program finished successfully";
         
         return 0;
     }
